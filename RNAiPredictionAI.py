3ac6c7f8303f80c195dd4ab5be56af644f120733
Toby Davis <129922345+SephDavis@users.noreply.github.com>
2023-04-12 22:38:43 -0500

    Update RNAiPredictionAI.py

--- RNAiPredictionAI.py
+++ RNAiPredictionAI.py
@@ -1,7 +1,10 @@
 import numpy as np
 from keras.models import Sequential
-from keras.layers import LSTM, Dense
+from keras.layers import LSTM, Dense, Bidirectional
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
 from sklearn.model_selection import train_test_split
+import sys
 
 # Define parameters
 sequence_length = 50
@@ -10,82 +13,74 @@ num_output_units = 1
 batch_size = 32
 num_epochs = 10
 
-def seq_to_one_hot(sequence):
-    one_hot = []
-    for char in sequence:
-        vec = [0, 0, 0, 0]
-        if char == 'A':
-            vec[0] = 1
-        elif char == 'C':
-            vec[1] = 1
-        elif char == 'G':
-            vec[2] = 1
-        elif char == 'T':
-            vec[3] = 1
-        one_hot.append(vec)
-    return one_hot
+# Beam search parameters
+beam_width = 10
 
-def load_multiple_fasta(filenames, labels):
-    seqs_and_one_hot = []
-    for idx, filename in enumerate(filenames):
-        with open(filename) as f:
-            f.readline()  # Skip header line
-            sequence = f.read().replace('\n', '')
-        one_hot_seq = seq_to_one_hot(sequence)
-        seqs_and_one_hot.append((sequence, one_hot_seq, labels[idx]))
-    return seqs_and_one_hot
+# Load data
+precursor_fasta = "precursor_sequences.fas"
+x_chromosome_fasta = "x_chromosome_sequence.fa"
 
-def predict_rnai_sequences(model, new_seq_file):
-    _, new_seq_one_hot, _ = load_fasta(new_seq_file, None)
-    predictions = model.predict(np.reshape(new_seq_one_hot[:-(len(new_seq_one_hot) % sequence_length)], (-1, sequence_length, 4)))
-    return predictions
+# Tokenize sequences
+tokenizer = Tokenizer(char_level=True)
+with open(precursor_fasta) as f:
+    data = f.read()
+tokenizer.fit_on_texts(data)
 
-# Load siRNA, shRNA, and DNA sequences
-siRNA_file = "siRNA_example.fasta"
-shRNA_file = "shRNA_example.fasta"
-DNA_file = "DNA_example.fasta"
-seqs_and_one_hot = load_multiple_fasta([siRNA_file, shRNA_file, DNA_file], [1, 1, 0])
+# Encode sequences
+def load_fasta(filename):
+    with open(filename) as f:
+        f.readline()  # Skip header line
+        sequence = f.read().replace('\n', '')
+    return tokenizer.texts_to_sequences([sequence])[0]
 
-# Create input/output data from one-hot encoded sequences
-input_data = []
-output_data = []
-for seq, one_hot_seq, label in seqs_and_one_hot:
-    for i in range(len(seq) - sequence_length - 1):
-        input_data.append(one_hot_seq[i:i+sequence_length])
-        output_data.append(label)
-input_data = np.reshape(input_data, (len(input_data), sequence_length, 4))
-output_data = np.array(output_data)
+precursor_seq = load_fasta(precursor_fasta)
+x_chromosome_seq = load_fasta(x_chromosome_fasta)
 
-# Split data into training and validation sets
-x_train, x_val, y_train, y_val = train_test_split(input_data, output_data, test_size=0.2, random_state=42)
+# Pad sequences
+def pad_sequence(seq, max_len):
+    return pad_sequences([seq], maxlen=max_len, padding='post', truncating='post')[0]
 
-# Define LSTM model
+input_data = pad_sequence(precursor_seq, sequence_length)
+x_chromosome_seq = pad_sequence(x_chromosome_seq, sequence_length)
+
+# Define bidirectional LSTM model
 model = Sequential()
-model.add(LSTM(num_hidden_units, return_sequences=True, input_shape=(sequence_length, 4)))
-model.add(LSTM(num_hidden_units, return_sequences=True))
-model.add(LSTM(num_hidden_units, return_sequences=True))
-model.add(LSTM(num_hidden_units))
+model.add(Bidirectional(LSTM(num_hidden_units, return_sequences=True), input_shape=(sequence_length, 1)))
+model.add(Bidirectional(LSTM(num_hidden_units, return_sequences=True)))
+model.add(Bidirectional(LSTM(num_hidden_units, return_sequences=True)))
+model.add(Bidirectional(LSTM(num_hidden_units)))
 model.add(Dense(num_output_units, activation='sigmoid'))
 
 # Compile model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# Prepare data for training
+x_train = np.array(input_data).reshape(-1, sequence_length, 1)
+y_train = np.array([1] * len(input_data))
+
 # Train model
-model.fit(x_train, y_train, validation_data=(x_val, y_val), batch_size=batch_size, epochs=num_epochs)
+model.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs)
 
-# Predict new siRNA and shRNA molecules
-new_seq_file = "new_sequence.fasta"
-predictions = predict_rnai_sequences
-(model, new_seq_file)
-predicted_rnai_sequences = []
-threshold = 0.5
+# Function to implement Beam search
+def beam_search(predictions, beam_width):
+    sequences = [[]]
+    for prediction in predictions:
+        all_candidates = []
+        for seq in sequences:
+            for idx, value in enumerate(prediction):
+                new_seq = seq.copy()
+                new_seq.append((idx, value))
+                all_candidates.append(new_seq)
 
-for i, prediction in enumerate(predictions):
-if prediction >= threshold:
-start_idx = i * sequence_length
-end_idx = (i + 1) * sequence_length
-predicted_rnai_sequences.append(new_seq_file[start_idx:end_idx])
+        ordered = sorted(all_candidates, key=lambda x: x[-1][1], reverse=True)
+        sequences = ordered[:beam_width]
 
-print("Predicted RNAi sequences:")
-for seq in predicted_rnai_sequences:
-print(seq)
+    return [seq[:-1] for seq in sequences]
+
+# Predict new miRNA sequences
+predictions = model.predict(np.array(x_chromosome_seq).reshape(-1, sequence_length, 1))
+beam_search_results = beam_search(predictions, beam_width)
+
+print("Predicted miRNA sequences:")
+for seq in beam_search_results:
+    print(tokenizer.sequences_to_texts([seq])[0])
